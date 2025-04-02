c1c7ea9c8f760fecfc447ce6a838cfde74a1ac7f
young030705 <young030705@naver.com>
2025-04-02 19:08:43 +0900

    modify

--- homework1.dart
+++ homework1.dart
@@ -1,3 +1,17 @@
+import 'dart:io';
 void main(){
-    print("Hello dddd Wolrd");
+    int x=int.parse(stdin.readLineSync()!);
+    int sum=0;
+    if(x>0)
+    {
+        while(x!=0)
+        {
+            sum+=x%10;
+            x= x~/10;
+        }
+        print(sum);
+    }
+    else
+        print(x);
+
 }
